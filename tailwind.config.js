f78f01641a90489ca3e4960a6989fb7b943c07a5
N10cap <younce92@gmail.com>
2024-09-11 16:07:22 +0200

    Update tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,51 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  darkMode: ['class'],
+  content: [
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
   theme: {
-    extend: {},
+    colors:{
+      'blue-primary': 'var(--color--blue-primary)',
+      'blue-light': 'var(--color--blue-linear-gradient)',
+      'black-blur': 'var(--color--black)',
+      'red-error': 'var(--color--red)',
+      'gray-200': 'var(--color--light-gray)',
+      'gray-300': 'var(--color--light-gray)',
+      'white': 'var(--color--white)',
+    },
+
+  	extend: {
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)',
+  		},
+  		colors: {},
+  		keyframes: {
+  			'accordion-down': {
+  				 from: {
+  					height: '0',
+  				},
+  				to: {
+  					height: 'var(--radix-accordion-content-height)',
+  				},
+  			},
+  			'accordion-up': {
+  				from: {
+  					height: 'var(--radix-accordion-content-height)',
+  				},
+  				to: {
+  					height: '0',
+  				},
+  			},
+  		},
+  		animation: {
+  			'accordion-down': 'accordion-down 0.2s ease-out',
+  			'accordion-up': 'accordion-up 0.2s ease-out',
+  		},
+  	},
   },
-  plugins: [],
-}
+  plugins: [require('tailwindcss-animate')],
+};
 
